8ab1f78120718a0c8b0a55adc00e87b241a51c87
Nenad Pesic <nenad.pesic32@gmail.com>
2020-04-02 19:06:03 +0200

    Twitter Clone

---
+++ tweetr-app/src/components/Auth/LogInForm.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="ui stackable three column centered grid container">
+    <div class="column">
+      <h2 class="ui dividing header">Log In</h2>
+
+      <Notification
+        :message="notification.message"
+        :type="notification.type"
+        v-if="notification.message"
+      />
+
+      <form class="ui form" @submit.prevent="login">
+        <div class="field">
+          <label>Email</label>
+          <input type="email" name="email" :class="{'input': true, 'is-danger': errors.has('email') }" v-model="email" v-validate="'required|email'" placeholder="Email">
+          <span v-show="errors.has('email')" class="is-danger">{{ errors.first('email') }}</span>
+        </div>
+
+        <div class="field">
+          <label>Password</label>
+          <input type="password" name="password" :class="{'input': true, 'is-danger': errors.has('password') }" v-model="password" v-validate="'required'" placeholder="Password" autocomplete="on">
+          <span v-show="errors.has('password')" class="is-danger">{{ errors.first('password') }}</span>
+        </div>
+
+        <button class="fluid ui primary button" :disabled="!isFormValid">LOG IN</button>
+
+        <div class="ui hidden divider"></div>
+      </form>
+
+      <div class="ui divider"></div>
+
+      <div class="ui column grid">
+        <div class="center aligned column">
+          <p>
+            Don't have an account? <router-link to="/signup">Sign Up</router-link>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+    import Notification from '@/components/Notification'
+
+    export default {
+        name: "LogInForm",
+        components: {
+            Notification
+        },
+        data () {
+            return {
+                email: '',
+                password: '',
+                notification: {
+                    message: '',
+                    type: ''
+                }
+            }
+        },
+        computed: {
+            isFormValid() {
+                return Object.keys(this.fields).every(key => this.fields[key].valid);
+            },
+        },
+        beforeRouteEnter (to, from, next) {
+            const token = localStorage.getItem('tweetr-token')
+            return token ? next('/') : next()
+        },
+        methods: {
+            login () {
+                axios
+                    .post('/login', {
+                        email: this.email,
+                        password: this.password,
+                    })
+                    .then(response => {
+                        localStorage.setItem('tweetr-token', response.data.data.token)
+                        this.$router.push('/')
+                    })
+                    .catch(error => {
+                        this.email = this.password = ''
+                        this.notification = Object.assign({}, this.notification, {
+                            message: error.response.data.message,
+                            type: error.response.data.status
+                        })
+                    })
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ tweetr-app/src/main.js
@@ -0,0 +1,27 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import axios from 'axios'
+import VeeValidate from 'vee-validate'
+import moment from 'moment'
+
+window.axios = axios;
+axios.defaults.baseURL = 'http://127.0.0.1:3333';
+
+Vue.use(VeeValidate)
+
+Vue.filter('timeAgo', date => moment(date).fromNow())
+Vue.filter('joined', date => moment(date).format('MMMM YYYY'))
+Vue.filter('dob', date => moment(date).format('MMMM Do YYYY'))
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
